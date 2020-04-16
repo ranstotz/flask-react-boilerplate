efc77b78768988f244bbb6d7ebdae3966dc9ec8f
Ryan Anstotz <32692901+ranstotz@users.noreply.github.com>
2020-04-15 22:26:00 -0400

    correct home route to serve resources.

--- app.py
+++ app.py
@@ -15,8 +15,12 @@ def items():
 @app.route('/<path:path>')
 def serve(path):
     ''' serve index.html for non-api routes '''
-    return render_template("index.html")
-
+    # if statement needed to accurately serve supporting files to index.html
+    if path != "" and os.path.exists(app.static_folder + '/' + path):
+        return send_from_directory(app.static_folder, path)
+    else:
+        # return send_from_directory(app.static_folder, 'index.html')
+        return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(use_reloader=True, port=5000, threaded=True)
